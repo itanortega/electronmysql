b82639785fae07978595463fbb776f380e1fc849
Itan Ortega <io@MacBook-Air-de-Itan.local>
2017-12-10 11:28:11 -0500

    punto de control

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const remote = require('electron').remote
 const main = remote.require('./main.js')
 
+let button = document.createElement('button')
+button.textContent = "Open Window"
+document.body.appendChild(button)
 
+button.addEventListener('click', ()=> {
+  main.openWindow()
+})
 
 $(function(){
   const os = require('os')
